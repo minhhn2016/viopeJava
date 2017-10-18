54867f383cc1bbdb44e4cd9fa43790e190776d88
minhhn2016 <minh.hn2016@gmail.com>
2017-10-18 12:35:54 +0300

    added ObjectsEmpDeptProgram

---
+++ viopeJava_chapter9_OOP_cont/src/viopeJava_chapter9_OOP_cont/Department.java
@@ -0,0 +1,27 @@
+package viopeJava_chapter9_OOP_cont;
+
+public class Department {
+    private int number;
+    private String name;
+
+    public Department(int number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
